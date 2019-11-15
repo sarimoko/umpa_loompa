3cc8ab7d04f7def999f6faa72005653dbc0569fb
Sarimoko <TravisReames@gmail.com>
2019-11-15 00:05:19 -0800

    Create app.sh

---
+++ app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "# UMPA Luumpa doopety doo..."dee
+echo "#    Less typing for me!"
+echo "# ===================================================================== #"
+echo "#"
+echo "#                                 U M P A"
+echo "#"
+echo "#      U b u n u t u   +   M y S Q L   +   P H P   +   A p a c h e      "
+echo "# ===================================================================== #"
+echo "# and for the doopeties of it..."
+echo "# * Postfix"
+echo "# * Mailutils"
+echo "# --------------------------------------------------------------------- #"
+echo "# installer | Promted through installers for: MySQL/PHP/Apache2
+echo "# "
+echo "# --------------------------------------------------------------------- #"
+read
